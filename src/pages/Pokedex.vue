b80fbf4e225e17be996d0b6a8a792171d436e56e
estebanmz <juanestebanmunoz.jems@gmail.com>
2026-08-06 12:19:56 -0500

    feat: Implement Slider and first time loading behavior

--- src/pages/Pokedex.vue
+++ src/pages/Pokedex.vue
@@ -1,7 +1,36 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onBeforeUnmount, onMounted } from 'vue'
+
+import Slider from '../components/onboarding/Slider.vue'
+import { globalStore } from '../store/global.store'
+
+const LOADING_DELAY_MS = 1900
+
+let loadingTimer: ReturnType<typeof window.setTimeout> | undefined
+
+onMounted(() => {
+  globalStore.setLoading(true)
+
+  loadingTimer = window.setTimeout(() => {
+    globalStore.setLoading(false)
+  }, LOADING_DELAY_MS)
+})
+
+onBeforeUnmount(() => {
+  if (loadingTimer) {
+    window.clearTimeout(loadingTimer)
+  }
+
+  globalStore.setLoading(false)
+})
+</script>
 
 <template>
   <div>POKEDEX</div>
+  <Slider
+    v-if="!globalStore.isOnboardingFinished"
+    @finish="globalStore.setOnboardingFinished(true)"
+  />
 </template>
 
 <style></style>

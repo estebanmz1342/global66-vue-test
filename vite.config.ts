d2f519cc5c1b62a1ee9c52a5d71f9e7a6f46f503
estebanmz <juanestebanmunoz.jems@gmail.com>
2026-08-06 18:28:24 -0500

    chore: Add alias imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'src'),
+    },
+  },
 })

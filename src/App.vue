35ef59e560dd39569f44a9cd92c99a9926e97274
estebanmz <juanestebanmunoz.jems@gmail.com>
2026-08-06 21:22:18 -0500

    feat: implement menu

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import { RouterView } from 'vue-router'
 
 import Loader from './components/global/Loader.vue'
 import { useGlobalStore } from './store/global.store'
+import Menu from './components/global/Menu.vue'
 
 const globalStore = useGlobalStore()
 </script>
 
 <template>
+  <Menu />
   <Loader v-if="globalStore.isLoading" />
   <RouterView />
 </template>
